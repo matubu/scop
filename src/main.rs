466a78f24cd82164aed692d9ac07616552d5879b
Mathias Berger--fruhinsholz <throw_new_Error@protonmail.com>
2022-05-28 18:55:56 +0200

    _

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
 extern crate glium;
+extern crate obj;
+
 use glium::{
 	{
 		Display,
+		Frame,
 		Surface,
-		IndexBuffer,
 		DrawParameters,
+		Program,
 		program,
+		implement_vertex,
 		uniform
 	},
+	uniforms::{
+		UniformBuffer
+	}
 	vertex::VertexBuffer,
 	index::{
+		NoIndices,
 		PrimitiveType::TrianglesList,
 	},
 	glutin::{
@@ -19,8 +27,77 @@ use glium::{
 		window::WindowBuilder
 	}
 };
+use obj::ObjData;
+use std::io::BufReader;
 
-mod teapot;
+#[derive(Copy, Clone)]
+struct Vertex {
+	position: [f32; 3],
+	texture: [f32; 2],
+	normal: [f32; 3]
+}
+
+implement_vertex!(Vertex, position, texture, normal);
+
+struct Object {
+	vertex_buffer: VertexBuffer<Vertex>
+}
+
+impl Object {
+	fn load(display: &Display, data: &[u8]) -> Self {
+		let data = ObjData::load_buf(BufReader::new(data)).unwrap();
+
+		let mut vertex_data = Vec::new();
+	
+		for object in data.objects {
+			for group in object.groups {
+				for polygon in group.polys {
+					for vertex in polygon.0.iter() {
+						let position = data.position[vertex.0];
+						let texture = match vertex.1 {
+							Some(i) => data.texture[i],
+							None => [0.0, 0.0]
+						};
+						let normal = match vertex.2 {
+							Some(i) => data.normal[i],
+							None => [0.0, 0.0, 0.0]
+						};
+	
+						vertex_data.push(Vertex {
+							position,
+							normal,
+							texture
+						})
+					}
+				}
+			}
+		}
+	
+		Self {
+			vertex_buffer: VertexBuffer::new(display, &vertex_data).unwrap()
+		}
+	}
+	fn draw(&self, frame: &Frame, program: Program) -> Self {
+		let params = DrawParameters {
+			depth: glium::Depth {
+				test: glium::draw_parameters::DepthTest::IfLess,
+				write: true,
+				.. Default::default()
+			},
+			.. Default::default()
+		};
+
+		frame.draw(
+			&self.vertex_buffer,
+			&NoIndices(TrianglesList),
+			&program,
+			&uniform! { matrix: MATRIX, light: LIGHT },
+			&params
+		).unwrap();
+
+		*self
+	}
+}
 
 fn main() {
 	let event_loop = EventLoop::new();
@@ -33,30 +110,7 @@ fn main() {
 				.with_gl(GlRequest::Specific(Api::OpenGl, (4, 1)));
 	let display = Display::new(win, ctx, &event_loop).unwrap();
 
-	const MATRIX: [[f32; 4]; 4] = [
-		[0.01, 0.0, 0.0, 0.0],
-		[0.0, 0.01, 0.0, 0.0],
-		[0.0, 0.0, 0.01, 0.0],
-		[0.0, 0.0, 0.0, 1.0]
-	];
-
-	const LIGHT: [f32; 3] = [-1.0, 0.4, 0.9];
-
-	let params = DrawParameters {
-		depth: glium::Depth {
-			test: glium::draw_parameters::DepthTest::IfLess,
-			write: true,
-			.. Default::default()
-		},
-		.. Default::default()
-	};
-
-	let positions = VertexBuffer::new(&display, &teapot::VERTICES).unwrap();
-	let normals = VertexBuffer::new(&display, &teapot::NORMALS).unwrap();
-	// let vertex_buffer = VertexBuffer
-	let indices = IndexBuffer::new(&display, TrianglesList,
-										&teapot::INDICES).unwrap();
-
+	let obj: Object = Object::load(&display, include_bytes!("../monkey.obj"));
 	let program = program!(&display,
 		150 => {
 			vertex: "
@@ -91,7 +145,29 @@ fn main() {
 		}
 	).unwrap();
 
+	const MATRIX: [[f32; 4]; 4] = [
+		[0.3, 0.0, 0.0, 0.0],
+		[0.0, 0.3, 0.0, 0.0],
+		[0.0, 0.0, -0.3, 0.0],
+		[0.0, 0.0, 0.0, 1.0]
+	];
+	const LIGHT: [f32; 3] = [-1.0, 0.4, 0.9];
+
+	const uniforms: UniformBuffer = uniform! { matrix: MATRIX, light: LIGHT };
+
 	event_loop.run(move |event, _, control_flow| {
+		let mut target = display.draw();
+		target.clear_color_and_depth((0.0, 0.0, 0.02, 1.0), 1.0);
+		obj.draw(&target, program, );
+		target.draw(
+				&vertex_buffer,
+				&NoIndices(TrianglesList),
+				&program,
+				&,
+				&params
+			).unwrap();
+		target.finish().unwrap();
+
 		match event {
 			Event::WindowEvent {
 				event: WindowEvent::CloseRequested,
@@ -99,17 +175,5 @@ fn main() {
 			} => *control_flow = ControlFlow::Exit,
 			_ => ()
 		}
-
-		let mut target = display.draw();
-		target.clear_color_and_depth((0.0, 0.0, 0.02, 1.0), 1.0);
-
-		target.draw(
-				(&positions, &normals),
-				&indices,
-				&program,
-				&uniform! { matrix: MATRIX, light: LIGHT },
-				&params
-		).unwrap();
-		target.finish().unwrap();
 	});
 }
